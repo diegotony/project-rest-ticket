e4676fa7afcc4c6b6e97c581da03763d190b3373
diegotony <tucotony1396@gmail.com>
2019-06-02 12:07:37 -0500

    migrate post

--- sequelize.js
+++ sequelize.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('sql9293155', 'sql9293155', 'WnWH4wMNmu', {
-    host: 'sql9.freesqldatabase.com',
-    dialect: 'mysql'
+const sequelize = new Sequelize('d4en0947khmmdd', 'hdzapgnajugejs', '3fc38c514b0274af8d0602bf36ee5b2d7385e7b0d57e09204a93bceb702e763e', {
+    host: 'ec2-75-101-147-226.compute-1.amazonaws.com',
+    dialect: 'postgres'
 });
 
 module.exports = sequelize
